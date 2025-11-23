992023e447987d41ec25a153655eb408de57e61c
Bagerca <bagrationbebia@gmail.com>
2025-11-23 17:48:44 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -15,7 +15,6 @@ const DOM = {
     duration: document.getElementById('duration'),
     currentTrack: document.getElementById('currentTrack'),
     currentArtist: document.getElementById('currentArtist'),
-    // albumImage удален, так как он больше не нужен напрямую (управляется через coverLoader)
     notificationContainer: document.getElementById('notificationContainer')
 };
 
@@ -368,6 +367,7 @@ export function updatePlaybackModeIcon(mode) {
 // --- ИКОНКА ГРОМКОСТИ ---
 export function updateVolumeIcon(volume) {
     const iconContainer = document.getElementById('volumeIcon');
+    const muteBtn = document.getElementById('muteBtn'); // Находим кнопку
     if (!iconContainer) return;
 
     // Очищаем старую иконку
@@ -378,15 +378,24 @@ export function updateVolumeIcon(volume) {
     if (volume == 0) {
         // Mute (Зачеркнутый)
         d = "M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73 4.27 3zM12 4L9.91 6.09 12 8.18V4z";
-    } else if (volume < 34) {
-        // Low (Только корпус)
-        d = "M7 9v6h4l5 5V4l-5 5H7z";
-    } else if (volume < 67) {
-        // Mid (Одна волна)
-        d = "M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z";
+        
+        // ДОБАВЛЯЕМ КЛАСС ДЛЯ КРАСНОГО ЦВЕТА
+        if (muteBtn) muteBtn.classList.add('muted');
+        
     } else {
-        // High (Максимум)
-        d = "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z";
+        // УБИРАЕМ КЛАСС ЕСЛИ ГРОМКОСТЬ ЕСТЬ
+        if (muteBtn) muteBtn.classList.remove('muted');
+
+        if (volume < 34) {
+            // Low (Только корпус)
+            d = "M7 9v6h4l5 5V4l-5 5H7z";
+        } else if (volume < 67) {
+            // Mid (Одна волна)
+            d = "M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z";
+        } else {
+            // High (Максимум)
+            d = "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z";
+        }
     }
 
     // Вставляем новый путь
